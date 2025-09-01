16b70de52b9633f334bdffd85bce665d0cef1211
lcsy1234 <2485531443@qq.com>
2025-09-01 23:57:10 +0800

    feat: 测试gp

--- apps/playground/src/App.tsx
+++ apps/playground/src/App.tsx
@@ -1,24 +1,9 @@
 // import { useEffect, useState, useRef } from "react";
 import ModalPlay from "./assets/component/modal";
-import { Select } from "lcsy-design";
 
 function App() {
-  const handleChange = (value: string) => {
-    console.log(`selected ${value}`);
-  };
   return (
     <>
-      <Select
-        defaultValue="lucy"
-        onChange={handleChange}
-        style={{ width: 120 }}
-        options={[
-          { value: "jack", label: "Jack" },
-          { value: "lucy", label: "Lucy" },
-          { value: "Yiminghe", label: "yiminghe" },
-          { value: "disabled", label: "Disabled", disabled: true },
-        ]}
-      />
       <ModalPlay></ModalPlay>
       {/* <Child onValueChange={setValue} />; */}
     </>
--- apps/playground/src/assets/component/select.tsx
+++ apps/playground/src/assets/component/select.tsx
@@ -1,4 +1,25 @@
-// import {Select} from "@lcsy-design/core"
-// import React, { useEffect, useState } from "react";
+// import { useEffect, useState, useRef } from "react";
+import { Select } from "lcsy-design";
 
+function SelectApp() {
+  const handleChange = (value: string) => {
+    console.log(`selected ${value}`);
+  };
+  return (
+    <>
+      <Select
+        defaultValue="lucy"
+        onChange={handleChange}
+        style={{ width: 120 }}
+        options={[
+          { value: "jasi", label: "Jasi" },
+          { value: "lucy", label: "Lucy" },
+          { value: "Yiminghe", label: "yiminghe" },
+          { value: "disabled", label: "Disabled", disabled: true },
+        ]}
+      />
+    </>
+  );
+}
 
+export default SelectApp;
